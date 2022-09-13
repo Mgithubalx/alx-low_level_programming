823bbf12f12ad817673c4a867b7bccaa3a110490
Mgithubalx <mhakamarian@gmail.com>
2022-09-13 12:28:56 -0700

    more fixes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,11 @@
  */
 int main(void)
 {
-	int c;
+	int d;
 
-	c = 0;
-	while
-		(c < 10)
-		{putchar("c");
-		c++;
-		}
+	for (d = '0' ; d <= '9' ; d++)
+		putchar("d");
 	putchar('\n');
+
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
@@ -19,4 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
