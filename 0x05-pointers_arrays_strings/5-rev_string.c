cd738cc62250fd6728d0581b1adec46a605b3a0d
Mgithubalx <mhakamarian@gmail.com>
2022-09-20 23:54:03 +0200

    tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,16 +3,29 @@
 #include "main.h"
 
 /**
- * rev_string - prints a string in reverse order
- * @s: String to reverse
- * Return: Nothing
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
-
 void rev_string(char *s)
 {
-        int len = strlen(s);
+	char tmp;
+	int i, len, len1;
 
-        while (len--)
-                putchar(*(s + len));
-        putchar(10);
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
+
