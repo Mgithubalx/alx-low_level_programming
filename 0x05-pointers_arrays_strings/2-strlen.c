50d1c709a3b3e59c543c09cf52c56c2a7ac38bf4
Mgithubalx <mhakamarian@gmail.com>
2022-09-21 00:26:17 +0200

    corrections

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
+ *
  * Returns: returns length as an integer;
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*(s + len) != '\0')
 		len++;
 
