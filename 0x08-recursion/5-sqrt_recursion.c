e1d4d5d5e6cee7e85fd7773bee900f0f94e0d872
Mgithubalx <mhakamarian@gmail.com>
2022-09-29 07:15:11 -0700

    recursion tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number with square root to be calculated
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (act_sqrt_recursion(n, 0));
+}
+
+/**
+ * act_sqrt_recursion - recurses to find the natural sqaure root of a number
+ * @n: number with square root to be calculated
+ * @x: squared number
+ *
+ * Return: square root of n
+ */
+int act_sqrt_recursion(int n, int x)
+{
+	if (x * x > n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (act_sqrt_recusion(n, x + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int prime_num(int n, int a);
+
+/**
+ * is_prime_number - returns 1 if input integer is a prime number
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime_num(n, n - 1));
+}
+
+/**
+ * prime_num - returns 1 if input integer is a prime number
+ * @n: number to evaluate
+ * @a: beginning of check
+ *
+ * Return 1 if n is prime, 0 otherwise
+ */
+int prime_num(int n, int a)
+{
+	if (a == 1)
+		return (1);
+	if (n % a == 0 && a > 0)
+		return (0);
+	return (prime_num(n, a - 1));
+}
